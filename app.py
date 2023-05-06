21f7c6db9e1e418c7742b47581edebe28e575bce
Isidora <vulic.isi@gmail.com>
2023-05-06 12:57:28 +0200

    Added calendar for setting and updating the date

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #postavljanje baze
+db = SQLAlchemy(app) #inicijalizacija baze
 
-class Book(db.Model):
+#kreiranje tabele u bazi
+
+class Book(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(50), nullable=False)
@@ -15,15 +17,16 @@ class Book(db.Model):
     def __repr__(self):
         return '<Book %r>' % self.id
     
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+ #   db.create_all()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         book_title = request.form['title']
         book_author = request.form['author']
-        new_book = Book(title=book_title, author=book_author)
+        book_published = datetime. strptime(request.form['published'], '%Y-%m-%d')
+        new_book = Book(title=book_title, author=book_author, published=book_published)
         try:
             db.session.add(new_book)
             db.session.commit()
@@ -51,6 +54,7 @@ def update(id):
     if request.method == 'POST':
         book_to_update.title = request.form['title']
         book_to_update.author = request.form['author']
+        book_to_update.published = datetime. strptime(request.form['published'], '%Y-%m-%d')
         try:
             db.session.commit()
             return redirect('/')
